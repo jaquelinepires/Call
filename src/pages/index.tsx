57b445456520fcb8845290ae1b92b0eeaee31847
Jaqueline <piresjaqueline17@gmail.com>
2023-02-22 20:17:06 -0300

    Setup & Structure - ESLint Configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { Heading } from '@ignite-ui/react'
 export default function Home() {
-  return (
-    <Heading as='h1'>hello World</Heading>
-  )
+  return <Heading as="h1">hello World</Heading>
 }
